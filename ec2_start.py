4e254cfa507f9e6edd89a3ed2bdca845f0d89fcc
Kandice Hendricks <kandicelhendricks@gmail.com>
2021-05-13 17:18:17 -0400

    Create ec2_start.py
    added new file

---
+++ ec2_start.py
@@ -0,0 +1,43 @@
+#### Put this in a Lambda function that is triggered by a CloudWatch scheduled rule
+#### This will start all EC2 instances with the tag name of AutoStart and a value of yes
+#### All tag names and values ARE case sensitive
+
+import boto3
+import logging
+
+#setup simple logging for INFO
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+#define the connection
+ec2 = boto3.resource('ec2')
+
+def lambda_handler(event, context):
+    # Use the filter() method of the instances collection to retrieve
+    # all running EC2 instances.
+    filters = [{
+            'Name': 'tag:AutoStart',
+            'Values': ['Yes']
+        },
+        {
+            'Name': 'instance-state-name', 
+            'Values': ['stopped']
+        }
+    ]
+    
+    #filter the instances
+    instances = ec2.instances.filter(Filters=filters)
+
+    #locate all stopped instances
+    StoppedInstances = [instance.id for instance in instances]
+    
+    print('the instances for logging purposes')
+    print(StoppedInstances) 
+    
+    #make sure there are actually instances to shut down. 
+    if len(StoppedInstances) > 0:
+        #perform the shutdown
+        startingUp = ec2.instances.filter(InstanceIds=StoppedInstances).start()
+        print('startingUp')
+    else:
+        print("Nothing to see here")
