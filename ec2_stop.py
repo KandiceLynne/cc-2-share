48bc1fd3ea215df3f0d2b1f2900bb119636df42e
Kandice Hendricks <kandicelhendricks@gmail.com>
2021-05-13 17:19:04 -0400

    Update ec2_stop.py
    added info

--- ec2_stop.py
+++ ec2_stop.py
@@ -1,3 +1,7 @@
+#### Put this in a Lambda function that is triggered by a CloudWatch scheduled rule
+#### This will start all EC2 instances with the tag name of AutoStop and a value of Yes
+#### All tag names and values ARE case sensitive
+
 import boto3
 import logging
 
